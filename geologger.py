481460bc1f7eeccdb2b6a1bc62278f8d4b1121aa
Jonah Duckles <jonahd@gmail.com>
2013-03-26 14:25:50 -0500

    More methods and cleaning things up.

--- geologger.py
+++ geologger.py
@@ -14,11 +14,17 @@ from util import *
 def getTagData(tagname, user_id="guest", db="geologger", col="lightlogs"):
     """ Get light level data for a tag """ 
     url = "http://test.cybercommons.org/mongo/db_find/%s/%s/{'spec':{'tagname':'%s','user_id':'%s'}}" %(db,col,tagname, user_id)
-    return json.loads(urllib.urlopen(url_fix(url)).read())[0]
+    url_get = urllib.urlopen(url_fix(url)).read()
+    if url_get == "[]":
+        return {"error": "Empty result"}
+    else:
+        return json.loads(url_get)[0]
 
 @task
-def importTagData( uploadloc, tagname, notes, location, user_id="guest" ):
+def importTagData( uploadloc, tagname, notes, location, user_id=None ):
     """ Import a geologger tag to mongodb """ 
+    if not user_id:
+        user_id = getTaskUser(importTagData.request.id)
     data = {
             "tagname":tagname, 
             "notes":notes, 
@@ -35,30 +41,55 @@ def importTagData( uploadloc, tagname, notes, location, user_id="guest" ):
         return "Error saving to mongodb"
 
 @task
-def twilightCalc( tagname=None, user_id=None, threshold=None ):
+def twilightCalc( tagname=None, threshold=None ):
     """ Python wrapper for GeoLight twilightCalc() """
     r = robjects.r
+    user_id = getTaskUser(twilightCalc.request.id)
     r.library('GeoLight')
     r.library('RJSONIO')
-    ligdata = dict2csv(getTagData(tagname,user_id),subkey="data")
-    r('lig <- read.csv("%s", header=T)' % ligdata)
-    r('trans <- twilightCalc(lig$datetime, lig$light, LightThreshold=%s, ask=F)' % threshold)
-    c = mongoconnect('geologger','twilights')
-    data = { 
-            "data":json.loads(r('toJSON(trans)')[0]), 
-            "tagname": tagname, 
-            "user_id": user_id, 
-            "threshold": threshold, 
-            "timestamp": datetime.datetime.now().isoformat()  
-            }
-    c.insert(data)
-    cleanup(ligdata)
-    return 'http://test.cybercommons.org/mongo/db_find/geologger/twilights/{"spec":{"tagname":"%s","user_id":"%s"}}' % (tagname, user_id)
+    tagdata = getTagData(tagname,user_id)
+    if tagdata != {"error": "Empty result"}:
+        ligdata = dict2csv(tagdata,subkey="data")
+        r('lig <- read.csv("%s", header=T)' % ligdata)
+        r('trans <- twilightCalc(lig$datetime, lig$light, LightThreshold=%s, ask=F)' % threshold)
+        c = mongoconnect('geologger','twilights') 
+        data = { 
+                "data":json.loads(r('toJSON(trans)')[0]), 
+                "tagname": tagname, 
+                "user_id": user_id, 
+                "threshold": threshold, 
+                "timestamp": datetime.datetime.now().isoformat(),
+                "format": "RJSONIO"
+                }
+        c.insert(data)
+        cleanup([ligdata])
+        return 'http://test.cybercommons.org/mongo/db_find/geologger/twilights/{"spec":{"tagname":"%s","user_id":"%s"}}' % (tagname, user_id)
+    else:
+        return "Had a problem finding lightlog data"
 
 @task
-def changeLight( tagname=None, user_id="guest", riseprob=None, setprob=None, days=None ):
+def twilightInsert(tagname=None, data=None, threshold=None):
+    """ Take twilight data from web interface """
+    c = mongoconnect('geologger','twilights')
+    user_id = getTaskUser(twilightInsert.request.id)
+    data = { 
+        "data": json.loads( data ), 
+        "tagname": tagname, 
+        "user_id": user_id, 
+        "threshold": threshold, 
+        "timestamp": datetime.datetime.now().isoformat(),
+        "format": "JSON-list"
+        }
+    c.save(data)
+    return 'http://test.cybercommons.org/mongo/db_find/geologger/twilights/{"spec":{"tagname":"%s","user_id":"%s"}}' % (tagname, user_id)
+
+    
+
+@task
+def changeLight( tagname=None, riseprob=None, setprob=None, days=None ):
     """ Python wrapper for GeoLight changeLight() """
     r = robjects.r
+    user_id = getTaskUser(changeLight.request.id)
     r.library('GeoLight')
     r.library('RJSONIO')
     twilight = df2csv(getTagData(tagname=tagname, user_id=user_id, col="twilights"), subkey="data")
@@ -85,7 +116,7 @@ def changeLight( tagname=None, user_id="guest", riseprob=None, setprob=None, day
             "timestamp": datetime.datetime.now().isoformat()
             }
     c.insert(data)
-    cleanup(twilight)
+    cleanup([twilight])
     return 'http://test.cybercommons.org/mongo/db_find/geologger/changelight/{"spec":{"tagname":"%s","user_id":"%s"}}' % (tagname, user_id)
 
 @task
@@ -94,8 +125,9 @@ def distanceFilter( transdata, elevation, distance ):
     pass
 
 @task
-def coord( tagname=None, elevation=None, user_id="guest" ):
+def coord( tagname=None, sunelevation=None ):
     """ Python wrapper for GeoLight coord() """
+    user_id = getTaskUser(coord.request.id)
     r = robjects.r
     r.library('GeoLight')
     r.library('RJSONIO')
@@ -105,22 +137,23 @@ def coord( tagname=None, elevation=None, user_id="guest" ):
     r('twilight <- read.csv("%s", header=T)' % (twilight))
     r('twilight$tFirst <- as.POSIXlt(twilight$tFirst, origin="1970-01-01")') # Convert to R Datetime
     r('twilight$tSecond <- as.POSIXlt(twilight$tFirst, origin="1970-01-01")') # Convert to R Datetime
-    r('coord <- coord(twilight$tFirst, twilight$tSecond, twilight$type, elevation = %s)' % elevation)
+    r('coord <- coord(twilight$tFirst, twilight$tSecond, twilight$type, elevation = %s)' % sunelevation)
     c = mongoconnect('geologger','coord')
     data = { 
             "data": json.loads(r('toJSON(coord)')[0]), 
-            "elevation": elevation, 
+            "sunelevation": sunelevation, 
             "tagname": tagname, 
             "user_name": user_name,
             "timestamp": datetime.datetime.now().isoformat()    
             }
     c.insert(data)
-    cleanup(twilight)
+    cleanup([twilight])
     return 'http://test.cybercommons.org/mongo/db_find/geologger/coord/{"spec":{"tagname":"%s","user_id":"%s"}}' % (tagname,user_id)
 
 @task
 def sunAngle( transdata=None, calib_start=None, calib_stop=None, release_location=None ):
     r = robjects.r
+    user_id = getTaskUser(twilightCalc.request.id)
     #task_id = sunAngle.request.id
     r.library('GeoLight')
     lat, lon = release_location
@@ -137,11 +170,44 @@ def sunAngle( transdata=None, calib_start=None, calib_stop=None, release_locatio
     return {"sunelevation": elev[0] }
 
 @task
-def getElevation( calibdata, xylocation ):
-    """ Wrapper for GeoLight getElevation """ 
-    task_id = getElevation.request.id
-    inputdata = dict2csv( calibdata )
-    subprocess.call(['RScript', 'geolight.R', inputdata, xylocation])
-    return task_id
+def getElevation_test( calib=None ):
+    r = robjects.r
+    user_id = getTaskUser(twilightCalc.request.id)
+    r.library('GeoLight')
+    r.library('RJSONIO')
+    lat, lon = calib['release_location']
+    twjson = stringsave(json.dumps(calib['data']))
+    r('twilights <- fromJSON(file("%s"))' % twjson)
+    r('elev <- getElevation(twilights$tFirst, twilights$tSecond, calib$type, known.coord=c(%s,%s), plot=F)' %(lon, lat) )
+    elev = r('elev')
+    return { "sunelevation": elev }
 
+@task
+def getElevation( data=None ):
+    """ Wrapper for GeoLight getElevation """ 
+    if isinstance(data,str):
+        data = json.loads(data)
+    r = robjects.r
+    user_id = getTaskUser(twilightCalc.request.id)
+    r.library('GeoLight')
+    r.library('RJSONIO')
+
+    lat, lon = data['release_location']
+    tagname = data['tagname']
+    #twilight = getTagData(tagname, user_id, col="twilights")
+    twilight = data['data']
+    #twformat = twilight['format']
+    """if twformat == "RJSONIO":
+        twjson = stringsave(json.dumps(twilight['data']))
+        r('twilights <- fromJSON(file("%s"))' % twjson)
+        r('twilights$tFirst <- as.POSIXlt(twilights$tFirst, origin="1970-01-01")') # Convert to R Datetime
+        r('twilights$tSecond <- as.POSIXlt(twilights$tFirst, origin="1970-01-01")') # Convert to R Datetime
+    elif twformat == "JSON-list":"""
+    twjson = dict2csv(twilight, subkey="data")
+    r('twilights <- read.csv("%s", header=T)' % twjson)
+    r('elev <- getElevation(twilights$tFirst, twilights$tSecond, twilights$type, known.coord=c(%s,%s), plot=F)' %(lon, lat) )
+    elev = r('elev')
+    data = { "user_id": user_id, "sunelevation": elev[0], "timestamp": datetime.datetime.now().isoformat() , "tagname": tagname }
+    return data
+    
 
